f286bdbfc4a5dd134b8d6848ac851989fe822d4f
Hajime Fujita <hajime.fujita@gmail.com>
2023-08-06 17:48:21 +0900

    いったん元に戻した

--- Makefile
+++ Makefile
@@ -8,7 +8,14 @@ stop:
 	docker compose -f docker-compose.dev.yml stop
 migrate:
 	docker compose -f docker-compose.dev.yml run python ./manage.py migrate
+migrations:
+	docker compose -f docker-compose.dev.yml run python ./manage.py makemigrations
 rm:
 	docker rm `docker ps -a -q`
 rmi:
 	docker rmi `docker images -a -q`
+all_clear:
+	docker compose -f docker-compose.dev.yml down
+	docker volume rm profilee.db.volume
+	find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+	find . -path "*/migrations/*.pyc" -delete
--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,31 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.utils.translation import gettext_lazy as _
 
 from .models import User
 
 
-# Register your models here.
-class MyUserChangeForm(UserChangeForm):
-    class Meta:
-        model = User
-        fields = '__all__'
+@admin.register(User)
+class AdminUserAdmin(UserAdmin):
 
-
-class MyUserCreationForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ('email',)
-
-
-class MyUserAdmin(UserAdmin):
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Permissions'), {
-         'fields': ('is_active', 'is_staff', 'is_superuser',)}),
-        (_('Important dates'), {
-         'fields': ('last_login', 'created_at', 'updated_at')}),
+        (_('Personal info'), {'fields': ('last_name', 'first_name', 'last_name_kana', 'first_name_kana', 'old_last_name',
+         'old_last_name_kana',  'sex', 'birthday', 'postal_code', 'prefecture', 'address', 'building', 'tel', 'url', 'photo')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = (
         (None, {
@@ -33,12 +22,47 @@ class MyUserAdmin(UserAdmin):
             'fields': ('email', 'password1', 'password2')
         })
     )
-    form = MyUserChangeForm
-    add_form = MyUserCreationForm
-    list_display = ('email', 'is_staff')
-    list_filter = ('is_staff', 'is_superuser', 'is_active',)
+    list_display = ('email', 'get_full_name', 'get_full_name_kana', 'sex', 'birthday',
+                    'postal_code', 'prefecture', 'address', 'building', 'tel', 'is_staff',)
     search_fields = ('email',)
-    ordering = ('email',)
+    ordering = ('date_joined',)
+    filter_horizontal = ('groups', 'user_permissions',)
+    exclude = ('username',)
 
 
-admin.site.register(User, MyUserAdmin)
+# # Register your models here.
+# class MyUserChangeForm(UserChangeForm):
+#     class Meta:
+#         model = User
+#         fields = '__all__'
+
+
+# class MyUserCreationForm(UserCreationForm):
+#     class Meta:
+#         model = User
+#         fields = ('email',)
+
+
+# class MyUserAdmin(UserAdmin):
+#     fieldsets = (
+#         (None, {'fields': ('email', 'password')}),
+#         (_('Permissions'), {
+#          'fields': ('is_active', 'is_staff', 'is_superuser',)}),
+#         (_('Important dates'), {
+#          'fields': ('last_login', 'created_at', 'updated_at')}),
+#     )
+#     add_fieldsets = (
+#         (None, {
+#             'classes': ('wide',),
+#             'fields': ('email', 'password1', 'password2')
+#         })
+#     )
+#     form = MyUserChangeForm
+#     add_form = MyUserCreationForm
+#     list_display = ('email', 'is_staff')
+#     list_filter = ('is_staff', 'is_superuser', 'is_active',)
+#     search_fields = ('email',)
+#     ordering = ('email',)
+
+
+# admin.site.register(User, MyUserAdmin)
