c9fe9de4d1717425c3723c4c9db6fe461bcbe938
Hajime Fujita <hajime.fujita@gmail.com>
2023-08-07 14:09:14 +0900

    いろいろあきらめた

--- src/users/models.py
+++ src/users/models.py
@@ -10,7 +10,6 @@ from django.utils.translation import gettext_lazy as _
 
 
 class UserManager(BaseUserManager):
-    use_in_migrations = True
 
     def _create_user(self, email, password, **extra_fields):
         email = self.normalize_email(email)
